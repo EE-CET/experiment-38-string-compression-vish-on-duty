dc8667ca58ee5ad98914c8ef603f0f276a92cce6
Vish_online <vishnuonline2218@gmail.com>
2026-04-13 12:59:53 +0530

    Update StringCompression.java

--- src/StringCompression.java
+++ src/StringCompression.java
@@ -1,9 +1,1 @@
-public class StringCompression {
-    
-        // TODO: Read the string
-        // TODO: Iterate through the string to count consecutive characters
-        // TODO: Append char and count to a StringBuilder
-        // TODO: Compare lengths of original vs compressed
-        // TODO: Print the shorter one (or compressed if equal length, depending on strict interpretation. Usually original if compressed is not strictly smaller, but prompt says "if compressed is longer")
-  
-}
+
